9a9e539ad667aa77461e1a0275e7a0f570b930dd
gb-lucas-duarte <lucas.duarte@guiabolso.com.br>
2019-08-16 19:54:34 -0300

    Inicio do Projeto

---
+++ src/main/java/skynet/br/chamadaAlunos/ChamadaAlunosApplication.java
@@ -0,0 +1,13 @@
+package skynet.br.chamadaAlunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadaAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadaAlunosApplication.class, args);
+	}
+
+}
